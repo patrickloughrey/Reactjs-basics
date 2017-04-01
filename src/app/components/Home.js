49445d1741993be68208dc3326328b59d7deb3b2
Patrick Loughrey <PatrickLoughrey@Patricks-MacBook-Pro-2.local>
2017-04-01 15:49:59 -0400

    Dynamically created data within components

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,9 +3,17 @@ import React from "react";
 export class Home extends React.Component {
 
     render() {
+        /* We are extending the 'React.Component' and <- has this property */
+        console.log(this.props);
+        var text = "This is your very first React.js application!";
         return(
             <div>
-                <p> I am a new component </p>
+                <h2> Welcome {this.props.name} ! </h2>
+                <p> {text} </p>
+                <h4> Hobbies: </h4>
+                    <ul>
+                      {this.props.user.hobbies.map((hobby) => <li>{hobby}</li>)}
+                    </ul>
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -8,6 +8,12 @@ import { Home } from "./components/Home";
 class App extends React.Component {
     /* Called by React.js whenever React thinks it needs to render the Component */
     render() {
+        var user = {
+            name: "Patrick",
+            age: "23",
+            hobbies: ["Weight-lifting", "Coding", "Basketball", "Football", "Golf", "Television Shows"]
+        };
+
         return(
             /* JSX: Javascript mixed with XML: You can only render one root element */
             <div className="container">
@@ -19,7 +25,7 @@ class App extends React.Component {
 
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1"> 
-                        <Home/>
+                        <Home user={user}/>
                     </div>
                 </div>
             </div>
