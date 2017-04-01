b7533f2e295044101c0e8cc414cab4c8d9acb947
Patrick Loughrey <PatrickLoughrey@Patricks-MacBook-Pro-2.local>
2017-04-01 14:56:57 -0400

    Added second component

---
+++ src/app/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Home extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <p> I am a new component </p>
+            </div>
+        );
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 
+/* Import component from Header.js */
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
 class App extends React.Component {
     /* Called by React.js whenever React thinks it needs to render the Component */
     render() {
@@ -9,7 +13,13 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1"> 
-                        <h1> Hello! </h1>
+                        <Header/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1"> 
+                        <Home/>
                     </div>
                 </div>
             </div>
